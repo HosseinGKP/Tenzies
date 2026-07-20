6a62b07d5e250141e87cf790fff7c2a326169194
HGK.2010 <hg939295@gmail.com>
2026-07-20 20:55:53 +0330

    Green Click is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
 import './styles/App.css'
+import Die from "./components/Die"
+import { useState } from 'react'
+import { nanoid } from "nanoid"
 
 function App(){
   
+  const [dice, setDice] = useState(generateAllNewDice())
   
+  function generateAllNewDice(){
+    return new Array(10)
+    .fill(0)
+    .map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }))
+    
+  }
+  
+  function rollDice(){
+    setDice(generateAllNewDice())
+  }
+
+  function hold(id){
+    setDice(prevDice => {
+      return prevDice.map((dieObj) => {
+        return dieObj.id === id ? 
+          {...dieObj, isHeld : !dieObj.isHeld} :
+          dieObj
+      })
+    })
+  }
+
+  const diceElement = dice.map(dieObj =>
+    <Die
+      key = {dieObj.id}
+      id = {dieObj.id}
+      value = {dieObj.value}
+      isHeld = {dieObj.isHeld}
+      hold = {() => hold(dieObj.id)}
+    />
+  )
+
+
   return(
     <>
-      <h1>Hello Man in here</h1>
+      <main>
+
+        <div className='dice-container'>
+        {diceElement}
+        </div>
+
+        <button onClick={rollDice} className='roll-button'>Roll</button>
+      </main>
     </>
   )
 }
